7fb4cd11ad402b5f9fe525fa4e4f564c4a779c3d
yaokui <bhshare.cn@gmail.com>
2023-11-08 10:17:30 +0800

    feat: 首次使用时从GitHub上下载模型文件

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ print(pre)
 print(pre_info)
 # {'text': '待预测文本', 'result': '积极', 'classes': 2, 'score': [0.3605044484138489, 0.009216712787747383, 0.6302788257598877]}
 ```
+- text: str 接收到的文本,
+- result: str 预测结果,
+- classes: int 情感类别序号： 0.消极 1.中性 2.积极,
+- score: list[double] 每个类别的预测得分
--- src/bhsenti/__init__.py
+++ src/bhsenti/__init__.py
@@ -4,11 +4,31 @@
 # Website: http://www.bhshare.cn
 from typing import Dict
 import numpy as np
+import requests
+from tqdm import tqdm
 from transformers import BertTokenizer
 from onnxruntime import InferenceSession
 import os
 
 model_path = os.path.join(os.path.dirname(__file__), "onnx")
+
+if not os.path.exists(model_path + "/model.onnx"):
+    url = 'https://media.githubusercontent.com/media/yaokui2018/bhsenti/master/src/bhsenti/onnx/model.onnx'
+    print(f"[bhsenti] 首次使用，需要下载模型，请稍候片刻...\n{url}")
+    response = requests.get(url, stream=True)
+    file_size = int(response.headers.get('content-length', 0))
+    with open(model_path + "/model.onnx.download", 'wb') as file, tqdm(
+            desc="正在下载模型文件",
+            total=file_size,
+            unit='B',
+            unit_scale=True,
+            unit_divisor=1024,
+    ) as bar:
+        for data in response.iter_content(chunk_size=1024):
+            file.write(data)
+            bar.update(len(data))
+    os.rename(model_path + "/model.onnx.download", model_path + "/model.onnx")
+
 tokenizer = BertTokenizer.from_pretrained(model_path)
 session = InferenceSession(model_path + "/model.onnx")
 
@@ -26,7 +46,12 @@ def predict_info(text: str) -> Dict:
     """
     判断文本的情感极性
     :param text: 待预测文本
-    :return: map
+    :return: {
+        "text": str 接收到的文本,
+        "result": str 预测结果,
+        "classes": int 情感类别序号： 0.消极 1.中性 2.积极,
+        "score": list[double] 每个类别的预测得分
+    }
     """
     inputs = tokenizer(text, return_tensors="np")
     for key in inputs:
